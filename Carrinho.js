81f073b356e0a647d07457acf66bd0f92f8640f2
MarcosMiyashiro <125094536+MarcosMiyashiro@users.noreply.github.com>
2023-04-28 21:37:34 -0300

    Update Carrinho.js

--- Carrinho.js
+++ Carrinho.js
@@ -1,11 +1,6 @@
 const produtosDisponiveis = ["camiseta", "calça", "sapato", "jaqueta"];
-
-
 const precos = [49.99, 80.00, 120.00, 150.00];
-
-
 const quantidades = [0, 0, 0, 0];
-
 
 function adicionarProdutoAoCarrinho() {
 
@@ -13,20 +8,12 @@ function adicionarProdutoAoCarrinho() {
   const nomeProduto = selectProduto.options[selectProduto.selectedIndex].value;
   const quantidade = document.getElementById("quantidade").valueAsNumber;
 
-
-
   const index = produtosDisponiveis.indexOf(nomeProduto); 
   if (index !== -1) { 
     quantidades[index] += quantidade; 
-
-    document.getElementById("valorTotal").innerHTML = `O valor total do carrinho é R$ ${calcularValorTotalCarrinho().toFixed(2)}.`;
-
+    document.getElementById("valorTotal").innerHTML = `O valor total do carrinho é R$ ${calcularValorTotalCarrinho().toFixed(2)}.
   }
 }
-
-
-
-
 
 function calcularValorTotalCarrinho() {
   let valorTotal = 0;
